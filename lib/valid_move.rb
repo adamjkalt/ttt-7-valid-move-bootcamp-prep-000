84b3694b1ac557d5fa2f5bb16de15993f2e6d8a4
Adam Kalt <adam.kalt.1@gmail.com>
2018-03-09 01:04:07 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,17 +1,9 @@
-def display_board(board)
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
-end
-
-def input_to_index(user_input)
-  return user_input.to_i - 1
-end
-
 # code your #valid_move? method here
 def valid_move
+  if position_taken
+   else
+   end
+end
 end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
